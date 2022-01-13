b2487c5fa80af16ccd6a8bfa2f64f9c8ecaca738
ShimonSamay <shimonsamay7@gmail.com>
2022-01-13 16:58:58 +0200

    Add project files.

---
+++ SchoolADO.Net/Controllers/api/TeacherController.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SchoolADO.Net.Models;
+
+namespace SchoolADO.Net.Controllers.api
+{
+    public class TeacherController : ApiController
+    {
+        static string conectionString = "Data Source=SHIMONSAMAY;Initial Catalog=SchoolDB-ADO;Integrated Security=True;Pooling=False";
+        public IHttpActionResult Get()
+        {
+            try
+            {
+                List<Teacher> teachersList = getAllTeachers(conectionString);
+                return Ok(new { teachersList });
+            }
+            catch (SqlException sqlerr)
+            {
+                return BadRequest(sqlerr.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+       
+        public IHttpActionResult Get(int id)
+        {
+            try
+            {
+                Teacher someTeacher = getTeacher(conectionString, id);
+                return Ok(new { someTeacher });
+            }
+            catch (SqlException sqlerr)
+            {
+                return BadRequest(sqlerr.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+       
+        public IHttpActionResult Post([FromBody] Teacher newTeacher)
+        {
+            try
+            {
+                addTeacher(conectionString, newTeacher);
+                List<Teacher> teacherList = getAllTeachers(conectionString);
+                return Ok(new { teacherList });
+            }
+            catch (SqlException sqlerr)
+            {
+                return BadRequest(sqlerr.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        
+        public IHttpActionResult Put(int id, [FromBody] Teacher teacher)
+        {
+            try
+            {
+                updateTeacher(conectionString, id, teacher);
+                List<Teacher> teacherList = getAllTeachers(conectionString);
+                return Ok(new { teacherList });
+            }
+            catch (SqlException sqlerr)
+            {
+                return BadRequest(sqlerr.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        
+        public IHttpActionResult Delete(int id)
+        {
+            try
+            {
+                deleteTeacher(conectionString, id);
+                List<Teacher> teacherList = getAllTeachers(conectionString);
+                return Ok(new { teacherList });
+            }
+            catch (SqlException sqlerr)
+            {
+                return BadRequest(sqlerr.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+
+
+
+
+        private List<Teacher> getAllTeachers (string connection)
+        {
+           
+                List<Teacher> TeachersList = new List<Teacher>();
+                using (SqlConnection DBconnection = new SqlConnection(connection))
+                {
+                    DBconnection.Open();
+                    string query = "SELECT * FROM Teachers";
+                    SqlCommand command = new SqlCommand(query, DBconnection);   
+                    SqlDataReader DATA  = command.ExecuteReader();
+                    if (DATA.HasRows)
+                  {
+                    while (DATA.Read())
+                    {
+                      TeachersList.Add(new Teacher(DATA.GetString(1),DATA.GetString(2),DATA.GetInt32(3),DATA.GetString(4)));
+                    }
+                    DBconnection.Close();
+                    return TeachersList;
+                  }
+                return TeachersList;
+
+                }
+           
+        }
+
+        private Teacher getTeacher(string connection , int id) { 
+        Teacher someTeacher = new Teacher();
+        using (SqlConnection DBconnection = new SqlConnection(connection))
+            {
+                DBconnection.Open();
+                string query = $@"SELECT * FROM Teachers WHERE Id = {id}";
+                SqlCommand command = new SqlCommand(query,DBconnection);
+                SqlDataReader DATA = command.ExecuteReader();
+                if (DATA.HasRows)
+                {
+                    while (DATA.Read())
+                    {
+                        someTeacher = new Teacher(DATA.GetString(1), DATA.GetString(2), DATA.GetInt32(3), DATA.GetString(4));
+                        
+                    }
+                    DBconnection.Close();
+                    return someTeacher;
+
+                    
+                }
+                return someTeacher;
+            }
+        }
+
+        private void addTeacher (string connection , Teacher someTeachr)
+        {
+            using (SqlConnection DBconnection = new SqlConnection(connection))
+            {
+                DBconnection.Open();
+                string query = $@"INSERT INTO Teachers (FirstName , LastName , Wage , BirthDate)
+                                 VALUES ('{someTeachr.FirstName}' , '{someTeachr.LastName}' , {someTeachr.Wage} , '{someTeachr.BirthDate}')";
+                SqlCommand command = new SqlCommand(query, DBconnection);
+                command.ExecuteNonQuery();
+                DBconnection.Close();
+            }
+        }
+
+        private void updateTeacher (string connection, int id, Teacher someTeachr)
+        {
+            using (SqlConnection DBconnection = new SqlConnection(connection))
+            {
+                DBconnection.Open();
+                string query = $@"UPDATE Teachers
+                                  SET FirstName = '{someTeachr.FirstName}' , LastName = '{someTeachr.LastName}' , Wage = {someTeachr.Wage} , BirthDate = '{someTeachr.BirthDate}'
+                                  WHERE Id = {id}";
+                SqlCommand command = new SqlCommand(query,DBconnection);
+                command.ExecuteNonQuery();
+                DBconnection.Close();
+            }
+        }
+
+        private void deleteTeacher (string connection , int id)
+        {
+            using(SqlConnection DBconnection = new SqlConnection(connection))
+            {
+                DBconnection.Open();
+                string query = $@"DELETE FROM Teachers WHERE Id = {id}";
+                SqlCommand command = new SqlCommand(query, DBconnection);
+                command.ExecuteNonQuery();
+                DBconnection.Close();
+
+            }
+        }
+
+
+
+    }
+}
---
+++ SchoolADO.Net/Models/Teacher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SchoolADO.Net.Models
+{
+    public class Teacher
+    {
+        public string FirstName;
+        public string LastName;
+        public int Wage;
+        public string BirthDate;
+
+        public Teacher ( string _firstname ,string _lastname , int _wage , string _birthdate ) {
+        this.FirstName = _firstname;    
+        this.LastName = _lastname;
+        this.Wage = _wage;
+        this.BirthDate = _birthdate;
+        }
+
+        public Teacher () { }
+    }
+}
